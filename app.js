5173622f681d673c3b1ea1a3bc01739a4903317f
ElangCahyaMaulid <121102876+ElangCahyaMaulid@users.noreply.github.com>
2026-03-10 22:47:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-// JANGAN PERNAH LAKUKAN INI DI DUNIA NYATA
-const AWS_SECRET_KEY = "AKIAIMORERAHASIA1234567890EXAMPLEKEY";
+// Simulasi Database Connection
+const db_config = {
+    host: "localhost",
+    user: "admin",
+    password: "SUPER_SECRET_PASSWORD_123!", 
+    api_key: "SG.x7890123456789012345678901234567890" // Pola SendGrid Key
+};
+
+console.log("Mencoba koneksi ke database...");
